d67a1dbee8784e422a9f4c878a6d5b4693cda001
Arunfancraze <arun@fancraze.com>
2023-06-16 01:30:29 +0530

    whole basic jwt auth setup

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const authenticationMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("No token provided");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (e) {
+    throw new UnauthenticatedError("Not authorised to access the route");
+  }
+};
+
+module.exports = authenticationMiddleware;
